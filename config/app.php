8be5476ed82b9d932ba7110ce7d0e996b17eb8c8
arju88nair <arju88nair@gmail.com>
2018-07-24 21:32:21 +0530

    mongodb auth fix app/User.php

--- config/app.php
+++ config/app.php
@@ -160,6 +160,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Laravel\Socialite\SocialiteServiceProvider::class,
+        Jenssegers\Mongodb\MongodbServiceProvider::class,
+
 
 
     ],
@@ -211,6 +213,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Socialite' => Laravel\Socialite\Facades\Socialite::class,
+        'Moloquent'       => Jenssegers\Mongodb\Eloquent\Model::class,
 
     ],
 
