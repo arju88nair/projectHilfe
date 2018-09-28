479e9aa35cdbd052eed2e63fc87cd435b62910c0
arju88nair <arju88nair@gmail.com>
2018-09-28 23:00:17 +0530

    basic repo save

--- app/Http/Controllers/apiController.php
+++ app/Http/Controllers/apiController.php
@@ -18,25 +18,41 @@ class apiController extends Controller
         $path = $request["path"];
         $parsedDetails = rtrim(str_replace(basename($_SERVER['SCRIPT_NAME']), '', $path), '/');
         $client = new Client(); //GuzzleHttp\Client        $url='https://api.github.com/repos'.$parsedDetails;
-        $url='https://api.github.com/repos'.$parsedDetails;
-        $result = $client->get($url);
-        if($result->getStatusCode() != 200)
-        {
-            return array('code'=>500,'message'=>'Wrong URL');
+//        $url='https://api.github.com/repos'.$parsedDetails;
+        $url = 'https://api.github.com/repos/arju88nair/FuberApi';
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('User-Agent: Nair'));
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        $data = json_decode($data, true);
+        if (array_key_exists('message', $data)) {
+            return response()->json(['message' => 'error'], 401);
         }
-        else{
+        $data=(object)$data;
+        $repo= new Repo();
+        $repo->title=$data->name;
+        $repo->username=$data->owner['login'];
+        $repo->avatar_url=$data->owner['avatar_url'];
+        $repo->url=$data->html_url;
+        $repo->description=$data->description;
+        $repo->repo_created_at=$data->created_at;
+        $repo->pushed_at=$data->pushed_at;
+        $repo->subscribers_count=$data->subscribers_count;
+        $repo->language=$data->language;
+        $repo->forks=$data->forks;
+        $repo->stargazers_count=$data->stargazers_count;
+        return $repo->save();
 
-            $repo= new Repo();
-            $repo->url= $url;
-            $repo->save();
-            return $repo;
-            return array('code'=>200,'message'=>'Success','repo'=>$repo);
 
-        }
+        return response()->json(['message' => $repo], 200);
 
-        return Repo::getRepoDetails($request);
 
     }
+
     public function login(Request $request)
     {
         $user = $request->user();
